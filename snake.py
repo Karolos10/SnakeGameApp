5f6331bcd948ddfc80c0be52c041c0268929eb11
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-08-18 18:53:53 -0500

    Creation of snake's head, configuration of keyboard movements

--- snake.py
+++ snake.py
@@ -1,9 +1,68 @@
 import turtle
+import time
 
+posp = 0.1
+ 
 wn = turtle.Screen()
 wn.title("Snake Game")
 wn.bgcolor("black")
 wn.setup(width=600, height=600)
 wn.tracer(0)
+
+
+#Snake Head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+#Functions
+
+def up():
+    head.direction = "up"
+    
+def down():
+    head.direction = "down"
+    
+def left():
+    head.direction = "left"
+    
+def right():
+    head.direction = "right"
+    
+def mov():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+#Ketboard
+wn.listen()
+wn.onkeypress(up, "Up")
+wn.onkeypress(down, "Down")
+wn.onkeypress(right, "Right")
+wn.onkeypress(left, "Left")
+
+while True:
+    wn.update()
+    
+    mov()
+    time.sleep(posp)
+    
+
+
+
+
 
 wn.mainloop()
